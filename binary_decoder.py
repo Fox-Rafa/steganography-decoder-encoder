02cf762b615ba1d4aa01910c304f0da857a17508
Rafael Fox <Fox-Rafa@users.noreply.github.com>
2020-08-13 08:40:54 -0300

    Add files via upload

---
+++ binary_decoder.py
@@ -0,0 +1,222 @@
+def letterToDecimal(letter):
+	if letter == 'a' :
+		return('01100001')
+
+	if letter == 'b' :
+		return('01100010')
+
+	if letter == 'c' :
+		return('01100011')
+
+	if letter == 'd' :
+		return('01100100')
+
+	if letter == 'e' :
+		return('01100101')
+
+	if letter == 'f' :
+		return('01100110')
+
+	if letter == 'g' :
+		return('01100111')
+
+	if letter == 'h' :
+		return('01101000')
+
+	if letter == 'i' :
+		return('01101001')
+
+	if letter == 'j' :
+		return('01101010')
+
+	if letter == 'k' :
+		return('01101011')
+
+	if letter == 'l' :
+		return('01101100')
+
+	if letter == 'm' :
+		return('01101101')
+
+	if letter == 'n' :
+		return('01101110')
+
+	if letter == 'o' :
+		return('01101111')
+
+	if letter == 'p' :
+		return('01110000')
+
+	if letter == 'q' :
+		return('01110001')
+
+	if letter == 'r' :
+		return('01110010')
+
+	if letter == 's' :
+		return('01110011')
+
+	if letter == 't' :
+		return('01110100')
+
+	if letter == 'u' :
+		return('01110101')
+
+	if letter == 'v' :
+		return('01110110')
+
+	if letter == 'w' :
+		return('01110111')
+
+	if letter == 'x':
+		return('01111000')
+
+	if letter == 'y' :
+		return('01111001')
+
+	if letter == 'z' :
+		return('01111010')
+
+	if letter == '0' :
+		return('00110000')
+
+	if letter == '1' :
+		return('00110001')
+
+	if letter == '2' :
+		return('00110010')
+
+	if letter == '3' :
+		return('00110011')
+
+	if letter == '4' :
+		return('00110100')
+
+	if letter == '5' :
+		return('00110101')
+
+	if letter == '6' :
+		return('00110110 ')
+
+	if letter == '7' :
+		return('00110111')
+
+	if letter == '8' :
+		return('00111000')
+
+	if letter == '9' :
+		return('00111001')
+
+	if letter == ' ' :
+		return('00100000')
+
+def	bynarriToDecimal(byte):
+	if byte == '01100001' :
+		return('a')
+	if byte == '01100010' :
+		return('b')
+
+	if byte == '01100011' :
+		return('c')
+
+	if byte == '01100100' :
+		return('d')
+
+	if byte == '01100101' :
+		return('e')
+
+	if byte == '01100110' :
+		return('f')
+
+	if byte == '01100111' :
+		return('g')
+
+	if byte == '01101000' :
+		return('h')
+
+	if byte == '01101001' :
+		return('i')
+
+	if byte == '01101010' :
+		return('j')
+
+	if byte == '01101011' :
+		return('k')
+
+	if byte == '01101100' :
+		return('l')
+
+	if byte == '01101101' :
+		return('m')
+
+	if byte == '01101110' :
+		return('n')
+
+	if byte == '01101111' :
+		return('o')
+
+	if byte == '01110000' :
+		return('p')
+
+	if byte == '01110001' :
+		return('q')
+
+	if byte == '01110010' :
+		return('r')
+
+	if byte == '01110011' :
+		return('s')
+
+	if byte == '01110100' :
+		return('t')
+
+	if byte == '01110101' :
+		return('u')
+
+	if byte == '01110110' :
+		return('v')
+
+	if byte == '01110111' :
+		return('w')
+
+	if byte == '01111000' :
+		return('x')
+
+	if byte == '01111001' :
+		return('y')
+
+	if byte == '01111010' :
+		return('z')
+
+	if byte == '00110000' :
+		return('0')
+
+	if byte == '00110001' :
+		return('1')
+
+	if byte == '00110010' :
+		return('2')
+
+	if byte == '00110011' :
+		return('3')
+
+	if byte == '00110100' :
+		return('4')
+
+	if byte == '00110101' :
+		return('5')
+
+	if byte == '00110110 ' :
+		return('6')
+
+	if byte == '00110111' :
+		return('7')
+
+	if byte == '00111000' :
+		return('8')
+
+	if byte == '00111001' :
+		return('9')
+
+	if byte == '00100000' :
+		return(' ')
---
+++ steg1.py
@@ -0,0 +1,97 @@
+from PIL import Image, ImageDraw
+from binary_decoder import letterToDecimal
+from binary_decoder import bynarriToDecimal
+
+
+print('do you wnat to encode or decode?')
+enDe = input()
+
+if enDe == 'encode':
+
+
+	print('what will the image file be caled?')
+	imageName = input()
+	image = Image.new('RGB', (160,160), color='white')
+	imageName = imageName + '.png'
+	image.save(imageName)
+
+
+	print('what will you like to write')
+	output = ''
+	Input = input()
+	Input = str(Input)
+	for letr in Input:
+		answer = letterToDecimal(letr)
+		output = output+answer
+
+	print(output)
+	g = len(output)/3
+	gg = g + 1
+	gg = int(gg)
+	#print (gg)
+	#print(len(output))
+	pixels = image.load()
+	pixels[0, 0] = (gg, 2, 2)
+	h = 0
+
+	for p in range(gg):
+	#    pixels[1,p+1] = (int(output[h]), int(output[h+1]), int(output[h+2]))
+	#    print(int(output[h]), int(output[h+1]), int(output[h+2]))
+		k = len(output) - h
+	#    print('-', k)
+		if k == 1 :
+			pixels[int(p)+1, 0] = (int(output[h]), 2, 2)
+		if k == 2 :
+			pixels[int(p)+1, 0] = (int(output[h]), int(output[h+1]), 2)
+		if k > 2 :
+			pixels[int(p)+1, 0] = (int(output[h]), int(output[h+1]), int(output[h+2]))
+		h = h + 3
+
+	image.save(imageName)
+	image.show()
+
+#-----------------------------------------------------------------
+
+if enDe == 'decode':
+	print('what image will you like to decode? (dont add the image suffix)')
+	deImage = input()
+	deImage = deImage+'.png'
+	img = Image.open(deImage)
+	pixel = img.load()
+	n, n2, n3 = pixel[0,0]
+	bits = ''
+	for l in range(n):
+		r, g, b = pixel[int(l+1),0]
+		bits = bits+str(r)
+		bits = bits+str(g)
+		bits = bits+str(b)
+
+	bits = bits.replace('2','')
+	print(bits)
+	x = bits
+	ln = len(x)
+	ln = int(ln)
+	u = ln/8
+	u = int(u)
+	awnser = 'nada'
+	finalawnser = ''
+	for i in range(u):
+		al = (i+1)*8
+		y = x[al-8]
+		y = y+(x[al-7])
+		y = y+(x[al-6])
+		y = y+(x[al-5])
+		y = y+(x[al-4])
+		y = y+(x[al-3])
+		y = y+(x[al-2])
+		y = y+(x[al-1])
+		
+		output = bynarriToDecimal(y)
+
+		finalawnser = finalawnser + output
+	print(finalawnser)
+
+
+
+
+
